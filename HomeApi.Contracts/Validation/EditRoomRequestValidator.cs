c967aef69f1e39e83440c426ff4f2c1a2964daff
Alex <AleKsusha@DESKTOP-G48TPNV>
2021-12-21 22:01:15 +0300

    Add project files.

---
+++ HomeApi.Contracts/Validation/EditRoomRequestValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using HomeApi.Contracts.Models.Rooms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeApi.Contracts.Validation
+{
+    public class EditRoomRequestValidator : AbstractValidator<EditRoomRequest>
+    {
+        /// <summary>
+        /// Метод, конструктор, устанавливающий правила
+        /// </summary>
+        public EditRoomRequestValidator()
+        {
+            RuleFor(x => x.NewName).NotEmpty().Must(BeSupported)
+                .WithMessage($"Please choose one of the following locations: {string.Join(", ", Values.ValidRooms)}");
+            RuleFor(x => x.NewArea).NotEmpty();
+            RuleFor(x => x.NewVoltage).NotEmpty().InclusiveBetween(0, 220).WithMessage($"Please choose voltage between 0 and 220");
+            RuleFor(x => x.NewGasConnected).NotNull();
+        }
+
+        /// <summary>
+        ///  Метод кастомной валидации имени комнаты
+        /// </summary>
+        private bool BeSupported(string location)
+        {
+            return Values.ValidRooms.Any(e => e == location);
+        }
+    }
+}
